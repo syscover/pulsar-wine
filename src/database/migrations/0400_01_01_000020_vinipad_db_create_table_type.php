45c1b7b682f611e5793e8baad859e24f3db881f9
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-10-28 00:19:47 +0200

    init commit

---
+++ src/database/migrations/0400_01_01_000020_vinipad_db_create_table_type.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class VinipadDbCreateTableLang extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		if(! Schema::hasTable('wine_type'))
+		{
+			Schema::create('admin_lang', function (Blueprint $table) {
+				$table->engine = 'InnoDB';
+
+                $table->integer('id')->unsigned();
+                $table->string('lang_id', 2);
+                $table->string('name');
+
+                $table->timestamps();
+                $table->softDeletes();
+
+                $table->primary(['id', 'lang_id'], 'pk01_vinipad_db_grape');
+                $table->foreign('lang_id', 'fk01_vinipad_db_grape')
+                    ->references('id')
+                    ->on('admin_lang')
+                    ->onDelete('restrict')
+                    ->onUpdate('cascade');
+			});
+		}
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::dropIfExists('vinipad_db_grape');
+	}
+}
