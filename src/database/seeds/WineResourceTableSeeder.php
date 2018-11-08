6ac096f7bd5da4a4eb39faab360a298d64130587
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-11-09 00:46:39 +0100

    add resources

--- src/database/seeds/WineResourceTableSeeder.php
+++ src/database/seeds/WineResourceTableSeeder.php
@@ -9,7 +9,15 @@ class WineResourceTableSeeder extends Seeder {
     {
         Resource::insert([
             ['id' => 'wine',                        'name' => 'Wine Package',                           'package_id' => 400],
-            ['id' => 'wine-wine',                   'name' => 'Wines',                                  'package_id' => 400]
+            ['id' => 'wine-wine',                   'name' => 'Wines',                                  'package_id' => 400],
+            ['id' => 'wine-winery',                 'name' => 'Wineries',                               'package_id' => 400],
+            ['id' => 'wine-appellation',            'name' => 'Appellations',                           'package_id' => 400],
+            ['id' => 'wine-family',                 'name' => 'Families',                               'package_id' => 400],
+            ['id' => 'wine-type',                   'name' => 'Types',                                  'package_id' => 400],
+            ['id' => 'wine-grape',                  'name' => 'Grapes',                                 'package_id' => 400],
+            ['id' => 'wine-pairing',                'name' => 'Pairings',                               'package_id' => 400],
+            ['id' => 'wine-awards',                 'name' => 'Awards',                                 'package_id' => 400],
+            ['id' => 'wine-presentation',           'name' => 'Presentations',                          'package_id' => 400]
         ]);
     }
 }
