e363ca0cdc211e98d353561f31e61a8d0e85690b
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-11-13 18:44:13 +0100

    minor fix

--- src/database/seeds/WinePresentationTableSeeder.php
+++ src/database/seeds/WinePresentationTableSeeder.php
@@ -8,15 +8,15 @@ class WinePresentationTableSeeder extends Seeder {
     public function run()
     {
         Presentation::insert([
-            ['id' => 1,     'lang_id' => 'es',      'name' => 'Botella media (37,5 cl)',            'data_lang' => ['es']],
-            ['id' => 2,     'lang_id' => 'es',      'name' => 'Botela (75 cl)',                     'data_lang' => ['es']],
-            ['id' => 3,     'lang_id' => 'es',      'name' => 'Magnun (1,5 l)',                     'data_lang' => ['es']],
-            ['id' => 4,     'lang_id' => 'es',      'name' => 'Doble Magnum (3 l)',                 'data_lang' => ['es']],
-            ['id' => 5,     'lang_id' => 'es',      'name' => 'Jeroboam (4,5 l)',                   'data_lang' => ['es']],
-            ['id' => 6,     'lang_id' => 'es',      'name' => 'Imperial (6 l)',                     'data_lang' => ['es']],
-            ['id' => 7,     'lang_id' => 'es',      'name' => 'Salmanazar (9 l)',                   'data_lang' => ['es']],
-            ['id' => 8,     'lang_id' => 'es',      'name' => 'Baltasar (12 l)',                    'data_lang' => ['es']],
-            ['id' => 9,     'lang_id' => 'es',      'name' => 'Nabucodonosor (15 l)',               'data_lang' => ['es']],
+            ['id' => 1,     'lang_id' => 'es',      'name' => 'Botella media (37,5 cl)',            'data_lang' => '["es"]'],
+            ['id' => 2,     'lang_id' => 'es',      'name' => 'Botela (75 cl)',                     'data_lang' => '["es"]'],
+            ['id' => 3,     'lang_id' => 'es',      'name' => 'Magnun (1,5 l)',                     'data_lang' => '["es"]'],
+            ['id' => 4,     'lang_id' => 'es',      'name' => 'Doble Magnum (3 l)',                 'data_lang' => '["es"]'],
+            ['id' => 5,     'lang_id' => 'es',      'name' => 'Jeroboam (4,5 l)',                   'data_lang' => '["es"]'],
+            ['id' => 6,     'lang_id' => 'es',      'name' => 'Imperial (6 l)',                     'data_lang' => '["es"]'],
+            ['id' => 7,     'lang_id' => 'es',      'name' => 'Salmanazar (9 l)',                   'data_lang' => '["es"]'],
+            ['id' => 8,     'lang_id' => 'es',      'name' => 'Baltasar (12 l)',                    'data_lang' => '["es"]'],
+            ['id' => 9,     'lang_id' => 'es',      'name' => 'Nabucodonosor (15 l)',               'data_lang' => '["es"]'],
         ]);
     }
 }
--- src/database/seeds/WineTypeTableSeeder.php
+++ src/database/seeds/WineTypeTableSeeder.php
@@ -8,12 +8,12 @@ class WineTypeTableSeeder extends Seeder {
     public function run()
     {
         Type::insert([
-            ['id' => 1,     'lang_id' => 'es',      'name' => 'Tinto',                  'data_lang' => ['es']],
-            ['id' => 2,     'lang_id' => 'es',      'name' => 'Blanco',                 'data_lang' => ['es']],
-            ['id' => 3,     'lang_id' => 'es',      'name' => 'Rosado',                 'data_lang' => ['es']],
-            ['id' => 4,     'lang_id' => 'es',      'name' => 'Espumoso',               'data_lang' => ['es']],
-            ['id' => 5,     'lang_id' => 'es',      'name' => 'Generoso',               'data_lang' => ['es']],
-            ['id' => 6,     'lang_id' => 'es',      'name' => 'Dulce',                  'data_lang' => ['es']],
+            ['id' => 1,     'lang_id' => 'es',      'name' => 'Tinto',                  'data_lang' => '["es"]'],
+            ['id' => 2,     'lang_id' => 'es',      'name' => 'Blanco',                 'data_lang' => '["es"]'],
+            ['id' => 3,     'lang_id' => 'es',      'name' => 'Rosado',                 'data_lang' => '["es"]'],
+            ['id' => 4,     'lang_id' => 'es',      'name' => 'Espumoso',               'data_lang' => '["es"]'],
+            ['id' => 5,     'lang_id' => 'es',      'name' => 'Generoso',               'data_lang' => '["es"]'],
+            ['id' => 6,     'lang_id' => 'es',      'name' => 'Dulce',                  'data_lang' => '["es"]'],
         ]);
     }
 }
