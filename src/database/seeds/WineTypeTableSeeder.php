03e5b84754c776c28be49a2a20998affbae8d4c6
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-11-30 14:22:16 +0100

    types

--- src/database/seeds/WineTypeTableSeeder.php
+++ src/database/seeds/WineTypeTableSeeder.php
@@ -8,12 +8,15 @@ class WineTypeTableSeeder extends Seeder {
     public function run()
     {
         Type::insert([
-            ['id' => 1,     'lang_id' => 'es',      'name' => 'Tinto',          'slug' => 'tinto',          'data_lang' => '["es"]'],
-            ['id' => 2,     'lang_id' => 'es',      'name' => 'Blanco',         'slug' => 'blanco',         'data_lang' => '["es"]'],
-            ['id' => 3,     'lang_id' => 'es',      'name' => 'Rosado',         'slug' => 'rosado',         'data_lang' => '["es"]'],
-            ['id' => 4,     'lang_id' => 'es',      'name' => 'Espumoso',       'slug' => 'espumoso',       'data_lang' => '["es"]'],
-            ['id' => 5,     'lang_id' => 'es',      'name' => 'Generoso',       'slug' => 'generoso',       'data_lang' => '["es"]'],
-            ['id' => 6,     'lang_id' => 'es',      'name' => 'Dulce',          'slug' => 'dulce',          'data_lang' => '["es"]'],
+            ['id' => 1,     'lang_id' => 'es',      'name' => 'Tinto',                  'slug' => 'tinto',                  'data_lang' => '["es"]'],
+            ['id' => 2,     'lang_id' => 'es',      'name' => 'Blanco',                 'slug' => 'blanco',                 'data_lang' => '["es"]'],
+            ['id' => 3,     'lang_id' => 'es',      'name' => 'Rosado',                 'slug' => 'rosado',                 'data_lang' => '["es"]'],
+            ['id' => 4,     'lang_id' => 'es',      'name' => 'Espumoso',               'slug' => 'espumoso',               'data_lang' => '["es"]'],
+            ['id' => 5,     'lang_id' => 'es',      'name' => 'Generoso',               'slug' => 'generoso',               'data_lang' => '["es"]'],
+            ['id' => 6,     'lang_id' => 'es',      'name' => 'Dulce',                  'slug' => 'dulce',                  'data_lang' => '["es"]'],
+            ['id' => 7,     'lang_id' => 'es',      'name' => 'Tinto crianza',          'slug' => 'tinto-crianza',          'data_lang' => '["es"]'],
+            ['id' => 8,     'lang_id' => 'es',      'name' => 'Tinto reserva',          'slug' => 'tinto-reserva',          'data_lang' => '["es"]'],
+            ['id' => 9,     'lang_id' => 'es',      'name' => 'Tinto gran reserva',     'slug' => 'tinto-gran-reserva',     'data_lang' => '["es"]'],
         ]);
     }
 }
